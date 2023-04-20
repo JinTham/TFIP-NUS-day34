f696be8511a6252c9c58769ccd87f3989f1eed24
Yeh Jin Tham <yehjintham@gmail.com>
2023-04-20 13:32:49 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,123 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, Subscription, firstValueFrom, lastValueFrom, map } from 'rxjs';
 import { Weather } from './models';
-import { map } from 'rxjs/internal/operators/map';
-import { Subscription } from 'rxjs/internal/Subscription';
 
 const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather'
-const WEATHER_API_KEY = '18d19c57e8616b76696499ab4515334e'
+
+// Remeber to redact your API key when you check in
+const WEATHER_API_KEY = '_YOUR_OPENWEATHERMAP_KEY_'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy{
-  title = 'day34';
+export class AppComponent implements OnInit, OnDestroy {
 
-  form!:FormGroup
-  weather$!:Subscription
-  result:Weather[] = []
+  form!: FormGroup
+  weather$!: Subscription
+  weatherObs$!: Observable<Weather[]>
+  weatherProm$!: Promise<Weather[]>
 
-  constructor (private fb:FormBuilder, private http:HttpClient) {}
+  result: Weather[] = []
+
+  // remember to import HttpClientModule
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
-      this.form = this.fb.group({
-        city: this.fb.control('',[Validators.required])
-      })
+    this.form = this.fb.group({
+      city: this.fb.control('', [ Validators.required ])
+    })
   }
 
   ngOnDestroy(): void {
+    this.weather$.unsubscribe()
+  }
+
+  getWeatherWithObservable() {
+    this.weatherObs$ = this.getWeather()
+  }
+
+  getWeatherWithPromise2() {
+
+    // converts the first value of the observable
+    // into a promise, lastValueFrom()
+    this.weatherProm$ = lastValueFrom(
+      // returns an observable
+      this.getWeather()
+    )
+  }
+
+  getWeatherWithPromise() {
+
+    // converts the first value of the observable
+    // into a promise, lastValueFrom()
+    lastValueFrom(
+      // returns an observable
+      this.getWeather()
+    ).then(v => {
+      console.info('resolved: ', v)
+      this.result = v
+    }).catch(err => {
+      console.error('>>> error: ', err)
+    })
+
+  }
+
+  getWeatherWithSubscription() {
+
+    // unsubscribe first before subscribing
+    if (this.weather$)
       this.weather$.unsubscribe()
+
+    this.getWeather()
+      .subscribe({
+        next: v => {
+          console.info('----- NEXT')
+          this.result = v
+        },
+        error: err => {
+          console.error('----- ERROR: ', err)
+        },
+        complete: () => {
+          console.warn('----- COMPLETE: ')
+        }
+      })
+
   }
 
   getWeather() {
     const city = this.form.value['city']
-    console.info(city)
+    console.info(`City: ${city}`)
 
-    // create query param
+    // create query params
     const params = new HttpParams()
-        .set('q',city)
-        .set('appid',WEATHER_API_KEY)
+        .set('q', city)
+        .set('units', 'metric')
+        .set('appid', WEATHER_API_KEY)
 
-    // .get returns an Observable
-    this.weather$ = this.http.get<Weather[]>(WEATHER_URL,{ params })
-        .pipe(
-          map((v:any) => {
-            // .main.temp
-            const temp = v['main']['temp']
-            // .weather
-            const weather = v['weather'] as any[]
-            return weather.map(w => {
-              return {
-                main: w['main'],
-                description: w['description'],
-                icon: w['icon'],
-                temperature: temp
-              } as Weather
-            })
+    // returns an observable
+    return this.http.get<Weather[]>(WEATHER_URL, { params })
+      .pipe(
+        map((v: any) => {
+          // .main.temp
+          const temp = v['main']['temp']
+          // .weather
+          const weather = v['weather'] as any[]
+          return weather.map(w => {
+            return {
+              // .weather[*].main
+              main: w['main'],
+              // .weather[*].description
+              description: w['description'],
+              // .weather[*].icon
+              icon: w['icon'],
+              temperature: temp
+            } as Weather
           })
-        )
-        .subscribe({
-          next: v => {
-            console.info('---------NEXT') 
-            console.info(v)
-            this.result = v},
-          error: err => {console.error('--------ERROR: ',err)},
-          complete: () => {console.info('----------COMPLETE: ')}
         })
-  
+      )
   }
-  
 }
